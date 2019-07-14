6f7efacfe6846bc4881c1190a7a3be6041a5a0a2
Marlon Mattos <marlonsilvademattos@gmail.com>
2019-07-14 18:56:20 -0300

    Testes de contrato da API de Reservas

--- src/main/java/br/com/targettrust/traccadastros/controller/ReservaController.java
+++ src/main/java/br/com/targettrust/traccadastros/controller/ReservaController.java
@@ -4,6 +4,7 @@ import br.com.targettrust.traccadastros.dto.ReservaDto;
 import br.com.targettrust.traccadastros.servicos.ReservaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +19,13 @@ public class ReservaController {
     private ReservaService reservaService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public HttpEntity<Long> criar(@Valid @RequestBody ReservaDto reserva) {
+    public ResponseEntity criar(@Valid @RequestBody ReservaDto reserva) {
         if (reserva == null) {
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(reservaService.reservarVeiculo(reserva));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(reservaService.reservarVeiculo(reserva));
     }
 
     @PostMapping(path = "/{id}/cancelar", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
