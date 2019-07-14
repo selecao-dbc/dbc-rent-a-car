5fb1b0abc1c78db5d26fec2e1e912783fa13bdb2
Marlon Mattos <marlonsilvademattos@gmail.com>
2019-07-14 18:34:37 -0300

    Evelução dos teste de edição de locação

--- src/test/java/br/com/targettrust/traccadastros/servicos/LocacaoServiceTest.java
+++ src/test/java/br/com/targettrust/traccadastros/servicos/LocacaoServiceTest.java
@@ -2,14 +2,11 @@ package br.com.targettrust.traccadastros.servicos;
 
 import br.com.targettrust.traccadastros.converter.LocacaoConverter;
 import br.com.targettrust.traccadastros.dto.LocacaoDto;
-import br.com.targettrust.traccadastros.dto.ReservaDto;
 import br.com.targettrust.traccadastros.entidades.Carro;
 import br.com.targettrust.traccadastros.entidades.Locacao;
-import br.com.targettrust.traccadastros.entidades.Reserva;
 import br.com.targettrust.traccadastros.exceptions.NegocioException;
 import br.com.targettrust.traccadastros.repositorio.LocacaoRepository;
 import br.com.targettrust.traccadastros.stub.LocacaoStub;
-import br.com.targettrust.traccadastros.stub.ReservaStub;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -72,5 +69,28 @@ public class LocacaoServiceTest {
         when(modeloService.modeloDisponivel(1L, locacaoDto.getDataInicial(), locacaoDto.getDataFinal())).thenReturn(true);
         when(veiculoService.definirVeiculoPorModelo(1L)).thenReturn(new Carro());
         locacaoService.editarLocacaoVeiculo(1L, locacaoDto);
+    }
+
+    @Test(expected = NegocioException.class)
+    public void editarLocacaoModeloInvalido() {
+        Locacao locacao = new Locacao();
+        locacao.setId(1L);
+        when(locacaoRepository.findById(1L)).thenReturn(Optional.of(locacao));
+        LocacaoDto locacaoDto = LocacaoStub.gerarLocacaoDto(1L);
+        when(modeloService.modeloDisponivel(1L, locacaoDto.getDataInicial(), locacaoDto.getDataFinal())).thenReturn(true);
+        when(veiculoService.definirVeiculoPorModelo(1L)).thenReturn(null);
+        locacaoService.editarLocacaoVeiculo(2L, locacaoDto);
+    }
+
+    @Test(expected = NegocioException.class)
+    public void editarLocacaorVeiculoIndisponivel() {
+        Locacao locacao = new Locacao();
+        locacao.setId(1L);
+        when(locacaoRepository.findById(2L)).thenReturn(Optional.of(locacao));
+        LocacaoDto locacaoDto = LocacaoStub.gerarLocacaoDto(2L);
+        when(modeloService.modeloDisponivel(2L, locacaoDto.getDataInicial(), locacaoDto.getDataFinal())).thenReturn(false);
+        when(veiculoService.definirVeiculoPorModelo(2L)).thenReturn(new Carro());
+        when(veiculoService.definirVeiculoPorModelo(2L)).thenReturn(new Carro());
+        locacaoService.editarLocacaoVeiculo(2L, locacaoDto);
     }
 }
