8d250b617f91378b0f8e8c578cdb7ada3067c003
Marlon Mattos <marlonsilvademattos@gmail.com>
2019-07-14 16:37:18 -0300

    Definição de retorno CREATED ao criar uma reserva

--- src/main/java/br/com/targettrust/traccadastros/controller/ReservaController.java
+++ src/main/java/br/com/targettrust/traccadastros/controller/ReservaController.java
@@ -4,9 +4,13 @@ import br.com.targettrust.traccadastros.dto.ReservaDto;
 import br.com.targettrust.traccadastros.servicos.ReservaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -24,7 +28,8 @@ public class ReservaController {
         if (reserva == null) {
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(reservaService.reservarVeiculo(reserva));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(reservaService.reservarVeiculo(reserva));
     }
-
 }
