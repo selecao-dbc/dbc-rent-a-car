effd39d66215a0d414a68c5ceb3c1a4e29a6c76d
Waldecleber <waldecleber@prodam.am.gov.br>
2019-08-03 15:39:27 -0400

    Alugar um carro com modelo inexistente

--- src/test/java/br/com/targettrust/traccadastros/servico/LocacaoServiceTest.java
+++ src/test/java/br/com/targettrust/traccadastros/servico/LocacaoServiceTest.java
@@ -2,13 +2,14 @@ package br.com.targettrust.traccadastros.servico;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -24,7 +25,6 @@ import br.com.targettrust.traccadastros.entidades.Modelo;
 import br.com.targettrust.traccadastros.entidades.Veiculo;
 import br.com.targettrust.traccadastros.repositorio.LocacaoRepository;
 import br.com.targettrust.traccadastros.repositorio.MarcaRepository;
-import br.com.targettrust.traccadastros.repositorio.ModeloRepository;
 import br.com.targettrust.traccadastros.repositorio.VeiculoRepository;
 import br.com.targettrust.traccadastros.servico.exception.VeiculoNaoEncontradoException;
 import br.com.targettrust.traccadastros.servico.impl.LocacaoServiceImpl;
@@ -35,6 +35,7 @@ import br.com.targettrust.traccadastros.servico.impl.LocacaoServiceImpl;
  * @date 2 de ago de 2019
  */
 @Sql(value = "/sql/initial_load.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/sql/load_table_veiculo.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @TestPropertySource("classpath:application.properties")
@@ -43,7 +44,7 @@ public class LocacaoServiceTest {
 	private static final String MARCA_NOME = "Fiat";
 	private static final String MODELO_NOME = "Palio 1.0";	
 	private static final String MODELO_VERSAO = "ECONOMY Fire Flex 8V";
-	private static final String CARRO_PLACA = "JPW-0101";
+	private static final String CARRO_PLACA = "JSQ-0101";
 	private static final String CARRO_COR = "Azul metalico";
 
 	@MockBean
@@ -51,12 +52,13 @@ public class LocacaoServiceTest {
 	
 	@Autowired
 	private MarcaRepository marcaRepository;
-	
-	@Autowired
-	private ModeloRepository modeloRepository;
-	
+		
 	@Autowired
 	private VeiculoRepository veiculoRepository;
+	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	 
 
 	private LocacaoService locacaoService;
 	
@@ -71,26 +73,11 @@ public class LocacaoServiceTest {
 	@Before
 	public void setUP() throws Exception {
 		locacaoService = new LocacaoServiceImpl(locacaoRepository, veiculoRepository);
-		
-		marca = marcaRepository.findByNome(MARCA_NOME);
-		
-		modelo = new Modelo();
-		modelo.setAno(2017);
-		modelo.setMarca(marca);
-		modelo.setNome(MODELO_NOME);
-		modelo.setVersao(MODELO_VERSAO);
-		
-		carro = new Carro();
-		carro.setAnoFabricacao(2014);
-		carro.setPlaca(CARRO_PLACA);
-		carro.setModelo(modelo);
-		carro.setCor(CARRO_COR);
-		carro.setPortas(4);
 				
-//		locacao = new Locacao();
-//		locacao.setDataInicial(LocalDate.of(2019, 8, 6));
-//		locacao.setDataFinal(LocalDate.of(2019, 8, 16));
-//		locacao.setValor(40d);
+		locacao = new Locacao();
+		locacao.setDataInicial(LocalDate.of(2019, 8, 6));
+		locacao.setDataFinal(LocalDate.of(2019, 8, 16));
+		locacao.setValor(40d);
 		     
 	}
 	
@@ -99,22 +86,37 @@ public class LocacaoServiceTest {
 		Optional<Veiculo> optional = veiculoRepository.findByNomeModelo(MODELO_NOME);
 		assertThat(optional.isPresent()).isTrue();
 		Veiculo veiculo = optional.get();
-		assertThat(veiculo.getPlaca()).isEqualTo("JSQ-0101");
+		assertThat(veiculo.getPlaca()).isEqualTo(CARRO_PLACA);
 	}
 	
 	
 	@Test
-	public void realizarLocacaoDeUmCarro() throws VeiculoNaoEncontradoException {
-		locacaoService.salvar(carro.getModelo(), locacao.getDataInicial(), locacao.getDataFinal(), locacao.getValor());		
+	public void realizarLocacaoDeUmCarro() throws Exception {
+		Optional<Veiculo> optional = veiculoRepository.findByNomeModelo(MODELO_NOME);
+		locacaoService.salvar(optional.get().getModelo(), locacao.getDataInicial(), locacao.getDataFinal(), locacao.getValor());		
 		verify(locacaoRepository).save(locacao);
 	}
 	
 	@Test
-	public void alugarCarroPorModeloEData() throws VeiculoNaoEncontradoException {
-		when(veiculoRepository.findByModelo(carro.getModelo())).thenReturn(Optional.of(carro));
-		locacaoService.salvar(carro.getModelo(), locacao.getDataInicial(), locacao.getDataFinal(), locacao.getValor());	
-		verify(locacaoRepository).save(locacao);
-		
+	public void alugarCarroPorModeloEData() throws Exception {
+		Optional<Veiculo> optional = veiculoRepository.findByNomeModelo(MODELO_NOME);
+		Veiculo veiculo = optional.get();
+		locacaoService.salvar(veiculo.getModelo(), locacao.getDataInicial(), locacao.getDataFinal(), locacao.getValor());	
+		verify(locacaoRepository).save(locacao);		
+	}
+	
+	@Test
+	public void alugarCarroIndisponivel() throws Exception {
+		Optional<Veiculo> optional = veiculoRepository.findByNomeModelo("Palio 1.8");
+		if (optional.isPresent()) {
+			Modelo modelo = optional.get().getModelo();
+			locacaoService.salvar(modelo, locacao.getDataInicial(), locacao.getDataFinal(), locacao.getValor());	
+			
+		} else {
+			expectedException.expect(VeiculoNaoEncontradoException.class);
+			expectedException.expectMessage("Não existe veiculo com este modelo ");
+			
+		}
 	}
 
 }
