77a4d06c3bade0ab6689e54fe5ecaa4863d65045
robertocarneiro <roberto_filho@atlantico.com.br>
2019-10-21 21:57:17 -0300

    Finalizado testes para o controller de locação para o método de create

---
+++ src/test/java/br/com/targettrust/traccadastros/util/TestUtils.java
@@ -0,0 +1,36 @@
+package br.com.targettrust.traccadastros.util;
+
+import br.com.targettrust.traccadastros.entidades.dto.LocacaoOuReservaDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TestUtils {
+
+    public static LocacaoOuReservaDTO mockLocacaoOuReservaDTO(String modelo, LocalDate dataInicial, LocalDate dataFinal) {
+        LocacaoOuReservaDTO locacaoOuReservaDTO = new LocacaoOuReservaDTO();
+        locacaoOuReservaDTO.setModelo(modelo);
+        locacaoOuReservaDTO.setDataInicial(dataInicial);
+        locacaoOuReservaDTO.setDataFinal(dataFinal);
+
+        return locacaoOuReservaDTO;
+    }
+
+    public static String convertLocacaoOuReservaDTOToJson(Object object) throws JsonProcessingException {
+        JavaTimeModule module = new JavaTimeModule();
+        LocalDateDeserializer localDateDeserializer =  new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        module.addDeserializer(LocalDate.class, localDateDeserializer);
+        ObjectMapper om = Jackson2ObjectMapperBuilder.json()
+                .modules(module)
+                .build();
+        return om.writeValueAsString(object);
+    }
+
+    private TestUtils() {}
+}
