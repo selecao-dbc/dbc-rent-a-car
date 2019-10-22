0bd989c30e801d6c01d370dbda1e505cd0eb329d
robertocarneiro <roberto_filho@atlantico.com.br>
2019-10-21 21:54:03 -0300

    Finalizado controller para reserva com os endpoints para criar, editar, cancelar e deletar por id

--- src/main/java/br/com/targettrust/traccadastros/controller/ReservaController.java
+++ src/main/java/br/com/targettrust/traccadastros/controller/ReservaController.java
@@ -1,13 +1,50 @@
 package br.com.targettrust.traccadastros.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.targettrust.traccadastros.entidades.Reserva;
+import br.com.targettrust.traccadastros.entidades.dto.LocacaoOuReservaDTO;
+import br.com.targettrust.traccadastros.service.ReservaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.http.ResponseEntity.notFound;
 
 @RestController
 @RequestMapping("reservas")
 public class ReservaController {
 
-	// TODO 1 Implementar métodos para criação, alteração e cancelamento de reserva
+    @Autowired
+    private ReservaService reservaService;
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<Reserva> create(@RequestBody @Valid @NotNull LocacaoOuReservaDTO reservaDTO) {
 
+        Reserva reserva = reservaService.save(null, reservaDTO);
+
+        return reserva != null ? ok(reserva) : notFound().build();
+    }
+
+    @PutMapping(value="/{id}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Reserva> update(@PathVariable("id") Long id,
+                                          @RequestBody @Valid @NotNull LocacaoOuReservaDTO reservaDTO){
+
+        Reserva reserva = reservaService.save(id, reservaDTO);
+
+        return reserva != null ? ok(reserva) : notFound().build();
+    }
+
+    @PutMapping(value="/{id}/cancel", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Reserva> cancel(@PathVariable("id") Long id) {
+        return reservaService.cancel(id).isPresent() ? ok().build() : notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") Long id) {
+        reservaService.deleteById(id);
+    }
 }
