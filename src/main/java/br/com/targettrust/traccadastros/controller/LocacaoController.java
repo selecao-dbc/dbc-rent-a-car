a2b7c8244b92b0f8fa9bf813b6ee86827142b544
robertocarneiro <roberto_filho@atlantico.com.br>
2019-10-21 21:53:12 -0300

    Finalizado controller para locação com os endpoints para criar, editar e deletar por id

---
+++ src/main/java/br/com/targettrust/traccadastros/controller/LocacaoController.java
@@ -0,0 +1,45 @@
+package br.com.targettrust.traccadastros.controller;
+
+import br.com.targettrust.traccadastros.entidades.Locacao;
+import br.com.targettrust.traccadastros.entidades.dto.LocacaoOuReservaDTO;
+import br.com.targettrust.traccadastros.service.LocacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.http.ResponseEntity.notFound;
+
+@RestController
+@RequestMapping("locacoes")
+public class LocacaoController {
+
+    @Autowired
+    private LocacaoService locacaoService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Locacao> create(@RequestBody @Valid @NotNull LocacaoOuReservaDTO locacaoDTO) {
+
+        Locacao locacao = locacaoService.save(null, locacaoDTO);
+
+        return locacao != null ? ok(locacao) : notFound().build();
+    }
+
+    @PutMapping(value="/{id}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Locacao> update(@PathVariable("id") Long id,
+                                          @RequestBody @Valid @NotNull LocacaoOuReservaDTO locacaoDTO){
+
+        Locacao locacao = locacaoService.save(id, locacaoDTO);
+
+        return locacao != null ? ok(locacao) : notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") Long id) {
+        locacaoService.deleteById(id);
+    }
+}
